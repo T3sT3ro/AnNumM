0456d547dac077ceac67a243886015885ca8dd88
Tooster <max@polarczyk.pl>
2018-10-10 01:46:25 +0200

    Add B1L1 z7.jl bitstring float to float purely arithmetic

---
+++ B1L1/z7.jl
@@ -0,0 +1,17 @@
+function stringfloat(str)
+    s = str[1] == '1' ? -1 : 1
+
+    e = 0
+    for i = 2:12
+        e = e<<1 + (str[i] == '1' ? 1 : 0)
+    end
+    e -= 1023 #BIAS
+    m = t = Float64(1)
+    for i = 13:64
+        t/=2; m += str[i] == '1' ? t : 0
+    end
+
+    s*m*2.0^e
+end
+
+bitstring(stringfloat("1000000001101101010000000000000000000000000000000000000001111101"))
